0a5eab30654f797e2634e0f5b34a27829e819472
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-04 12:16:21 +0000

    test

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import './globals.css';
 import { Analytics } from "@vercel/analytics/react"
 
--- components/lottieClient.jsx
+++ components/lottieClient.jsx
@@ -1,7 +1,9 @@
 "use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+const DynamicLottie = dynamic(() => import("lottie-react"), { ssr: false });
+
 
 export default function LottieClient({ animationData, className }) {
-  return <Lottie animationData={animationData} className={className} />;
+  return <DynamicLottie animationData={animationData} className={className} />;
 }
