000e3d8944f01b4d2f45345b42e84dd5eeece0e7
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-04 09:10:04 +0000

    Analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Pexmon LTD - Innovating Digital Solutions',
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-black text-white`}>{children}</body>
+      <body className={`bg-black text-white`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
