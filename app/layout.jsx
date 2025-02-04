77e0f9512b9f9acb552f6989e35e3bf034b0ca57
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-04 09:02:41 +0000

    Project reinitialized

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,16 @@
 import './globals.css';
-import Head from "next/head";
 
 export const metadata = {
   title: 'Pexmon LTD - Innovating Digital Solutions',
   description: 'A cutting-edge software company revolutionizing the digital space in Kenya, delivering mobile apps, web platforms and digital services to businesses and customers accross Kenya.',
   icons: {
-    icon: "/favicon.png",
+    icon: "/favicon.ico",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </Head>
       <body className={`bg-black text-white`}>{children}</body>
     </html>
   );
