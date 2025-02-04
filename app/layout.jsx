e1fa33310b1e769cc9064e0c8b975239eea28f7d
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-04 08:50:45 +0000

    Project reinitialized

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
