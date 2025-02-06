f4f51766a03506399b64a7b8679bd9d720bbe1e6
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-06 06:41:10 +0000

    Font dependency installed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,18 @@
 import './globals.css';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { Press_Start_2P, Roboto, Lato } from '@next/font/google';
+
+const fonts = [
+  Press_Start_2P({ weight: '400', subsets: ['latin'], display: 'swap' }),
+  Roboto({ weight: '400', subsets: ['latin'], display: 'swap' }),
+  Lato({ weight: '400', subsets: ['latin'], display: 'swap' })
+];
+
+const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
 
 export const metadata = {
   title: 'Pexmon LTD - Innovating Digital Solutions',
-  description: 'A cutting-edge software company revolutionizing the digital space in Kenya, delivering mobile apps, web platforms and digital services to businesses and customers accross Kenya.',
+  description: 'A cutting-edge software company revolutionizing the digital space in Kenya, delivering mobile apps, web platforms and digital services to businesses and customers across Kenya.',
   icons: {
     icon: "/favicon.ico",
   },
@@ -12,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-black text-white`}>
+      <body className={`${randomFont.className} bg-black text-white`}>
         {children}
         <Analytics />
       </body>
